e58a6b7a36bd48fd78fce331043851f337116d0d
Bianca Campos <biancancampos@gmail.com>
2018-09-13 11:34:37 -0700

    Add lab9

---
+++ Lab9/Foodtruck/Cook.h
@@ -0,0 +1,16 @@
+//
+//  Cook.h
+//  Foodtruck
+//
+//  Created by Bianca Campos on 2018-09-13.
+//  Copyright © 2018 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FoodTruck.h"
+
+@interface Cook : NSObject <FoodTruckDelegate>
+
+
+
+@end
---
+++ Lab9/Foodtruck/Javier.h
@@ -0,0 +1,13 @@
+//
+//  Javier.h
+//  Foodtruck
+//
+//  Created by Bianca Campos on 2018-09-13.
+//  Copyright © 2018 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FoodTruck.h"
+@interface Javier : NSObject <FoodTruckDelegate>
+
+@end
